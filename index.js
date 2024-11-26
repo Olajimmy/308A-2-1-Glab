e5cc14d9de2a4c86f95036bb1b262f72660dd7c6
olajimmy <overdedicated9@gmail.com>
2024-11-25 21:23:51 -0500

    initial program completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const adventurer = {
+  name: "Robin",
+  health: 10,
+  inventory: ["sword", "potion", "artifact"],
+  companion: {
+    name: "Leo",
+    type: "Cat",
+    companinion: {
+      name: "frank",
+      type: "flea",
+      belongings: ["small hat", "sun glasses"],
+    },
+  },
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  },
+};
+
+adventurer.roll();
